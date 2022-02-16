3255260fe6b08ceb3bb071fe47c20347b614f09f
abishek <abhisekgrg05@gmail.com>
2022-02-16 22:41:22 +0545

    little change

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 function myfunction() {
-  document.getElementById("heading").innerHTML = "<h3>Calculator Demo<h3>";
+  document.getElementById("heading").innerHTML = "<h3>Calculator Demo!<h3>";
+}
+
+function value1() {
+  document.getElementById("input").value += "1";
+}
+function value2() {
+  document.getElementById("input").value += "2";
+}
+function value3() {
+  document.getElementById("input").value += "3";
+}
+function value4() {
+  document.getElementById("input").value += "4";
+}
+function value5() {
+  document.getElementById("input").value += "5";
+}
+function value6() {
+  document.getElementById("input").value += "6";
+}
+function value7() {
+  document.getElementById("input").value += "7";
+}
+function value8() {
+  document.getElementById("input").value += "8";
+}
+function value9() {
+  document.getElementById("input").value += "9";
+}
+function value0() {
+  document.getElementById("input").value += "0";
 }
